2b813032d08a233c36e21d204337cdbd535f18df
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-06-05 15:06:58 -0700

    first commit for question 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,33 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+const student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 22,
+    location: 'New York'
+};
+const student2: Student = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 25,
+    location: 'New York'
+};
+const studentList: Student[] = [student1, student2];
+const table = document.createElement('table');
+const tableBody = document.createElement('tbody');
+studentList.forEach((student) => {
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tableBody.appendChild(row);
+});
+table.appendChild(tableBody);
+document.body.appendChild(table);
