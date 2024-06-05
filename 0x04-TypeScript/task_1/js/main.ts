bc259f456678fbe8083705956949dba1f5de5998
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-06-05 15:43:42 -0700

    first commit for question 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,7 +4,7 @@ interface Teacher {
     fullTimeEmployee: boolean;
     location: string;
     yearsOfExperience?: number;
-    [key: string]: any
+    [key: string]: any;
 }
 
 const teacher3: Teacher = {
@@ -31,8 +31,43 @@ const director1: Directors = {
 console.log(director1);
 
 
+// eslint-disable-next-line @typescript-eslint/class-name-casing
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
 console.log(printTeacher('john', 'doe'));
+
+
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+interface StudentClassConstructorInterface {
+    new(firstName: string, lastName: string): StudentClassInterface;
+}
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+function createStudent(cStudent: StudentClassConstructorInterface, firstName: string, lastName: string): StudentClassInterface {
+    return new cStudent(firstName, lastName);
+}
+
+const student1 = createStudent(StudentClass, 'John', 'Doe');
+console.log(student1);
